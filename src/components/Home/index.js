3c06cfcd2a3f4015254019646ad902ec84bbf1e9
Nick12Thomas <nikhilsthomas123@gmail.com>
2023-03-19 17:54:21 +0530

    done with home page just need to add logo

---
+++ src/components/Home/index.js
@@ -0,0 +1,62 @@
+import Logo from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetter'
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['i', 'k', 'h', 'i', 'l']
+  const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+  useEffect(() => {
+     setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+  return (
+    <>
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={Logo} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend Developer / Backend Developer</h2>
+        <Link to="/conatact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+    </>
+  )
+}
+export default Home
---
+++ src/components/Layout/index.js
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => {
+    return ( 
+        <div class="App">
+         <Sidebar />
+         <div className='page'>
+            <span className='tags top-tags'>&lt;body&gt;</span>
+
+            <Outlet />
+
+            <span className='tags bottom-tag-html'>&lt;/html&gt;</span>
+
+         </div>
+        </div>
+    )
+}
+export default Layout 

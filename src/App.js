9cda9e7d8be417a47a29c52a7a40ea218d1944f5
Nick12Thomas <nikhilsthomas123@gmail.com>
2023-11-08 22:46:45 +0530

    fixed the contact bug

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="contact" element={<Contact/>} />
           
           
       </Route>
